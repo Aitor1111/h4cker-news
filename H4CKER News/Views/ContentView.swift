b59c7339880d5d87923dde2e1e5c3c452fc4ebf5
Aitor Trujillo <zenji@MacBook-Air-de-Aitor.local>
2021-10-30 19:45:41 +0200

    first commit

---
+++ H4CKER News/Views/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  H4CKER News
+//
+//  Created by Aitor Trujillo on 12/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView{
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack{
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+            }
+            .navigationBarTitle("H4CKER News")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ H4CKER News/Views/DetailView.swift
@@ -0,0 +1,25 @@
+//
+//  DetailView.swift
+//  H4CKER News
+//
+//  Created by Aitor Trujillo on 13/10/21.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+       WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
+
+
